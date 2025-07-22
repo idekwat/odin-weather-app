d94b4b5c0d73a478a10aad2f45b54c0df17d506a
idekwat <184945414+idekwat@users.noreply.github.com>
2025-07-23 01:20:37 +0800

    added fetching of data from API to json

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
 //NVEND8EB8W4EV3QYLAKN68SNY
+//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY
 
+function getData() {
+    const inputLocation = document.getElementById("city").value;
+    fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + inputLocation + '?key=NVEND8EB8W4EV3QYLAKN68SNY', 
+        {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response);
+    });
+}
