ec6390eaf15069ed6c91a5628136f6f2f7eb6073
idekwat <184945414+idekwat@users.noreply.github.com>
2025-07-26 15:13:33 +0800

    added comments on scripts, edited readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # odin-weather-app
+
+Created a weather app which uses an API Key to get data from Visual Crossing
+No CSS additions for now 
+Will add CSS once I know how to design better looking websites for both web and mobile devices.
--- scripts.js
+++ scripts.js
@@ -1,31 +1,29 @@
-const submit = document.getElementById("submitButton");
+const submit = document.getElementById("submitButton"); //submit button
 
+//Checking if submit button is clicked or enter is pressed
 submit.addEventListener('click', e => {
-    e.preventDefault();
+    e.preventDefault(); //to prevent from posting on the browser
     getData();
 });
 
 function getData() {
+    //various elements
     const inputLocation = document.getElementById("city").value;
     const loc = document.getElementById('currentLocation');
     const time = document.getElementById('currentTime');
     const desc = document.getElementById('description');
     const temper = document.getElementById('currentTemp');
+
+    //fetching for API
     fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + inputLocation + '?key=NVEND8EB8W4EV3QYLAKN68SNY', 
         {mode: 'cors'})
     .then(function(response) {
         return response.json();
     })
     .then(function(response) {
-        console.log(response);
-        JSON.stringify(response);
-        /*        
-        <h1 id ="currentLocation"></h1>
-        <h2 id ="currentTime"></h2>
-        <h3 id ="description"></h3>
-        <h4 id ="currentTemp"></h4>
-        */
+        JSON.stringify(response); //Stringify is used because the response is still JSON
        
+        //posting content from JSON to the elements
        loc.textContent = response.resolvedAddress;
        time.textContent = response.currentConditions.datetime;
        desc.textContent = response.description;
