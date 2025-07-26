2b3714bcf0f026d3626f696495d124c215927e04
idekwat <184945414+idekwat@users.noreply.github.com>
2025-07-26 15:07:09 +0800

    finished task without any css additions

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
-//NVEND8EB8W4EV3QYLAKN68SNY
-//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY
+const submit = document.getElementById("submitButton");
+
+submit.addEventListener('click', e => {
+    e.preventDefault();
+    getData();
+});
 
 function getData() {
     const inputLocation = document.getElementById("city").value;
+    const loc = document.getElementById('currentLocation');
+    const time = document.getElementById('currentTime');
+    const desc = document.getElementById('description');
+    const temper = document.getElementById('currentTemp');
     fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + inputLocation + '?key=NVEND8EB8W4EV3QYLAKN68SNY', 
         {mode: 'cors'})
     .then(function(response) {
@@ -10,5 +18,17 @@ function getData() {
     })
     .then(function(response) {
         console.log(response);
+        JSON.stringify(response);
+        /*        
+        <h1 id ="currentLocation"></h1>
+        <h2 id ="currentTime"></h2>
+        <h3 id ="description"></h3>
+        <h4 id ="currentTemp"></h4>
+        */
+       
+       loc.textContent = response.resolvedAddress;
+       time.textContent = response.currentConditions.datetime;
+       desc.textContent = response.description;
+       temper.textContent = response.currentConditions.temp;
     });
 }
